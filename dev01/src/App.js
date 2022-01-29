6b3bfbd7df1f3bcca407b3237cd823e28dd356e0
hita03 <hitajuneja@gmail.com>
2022-01-29 17:13:44 +0530

    Init the app

--- dev01/src/App.js
+++ dev01/src/App.js
@@ -1,5 +1,5 @@
 
-import React, { Fragment,Component } from "react";
+import React, { Fragment,Component, useEffect, useState } from "react";
 import grapesjs from 'grapesjs';
 import 'grapesjs/dist/css/grapes.min.css';
 import 'grapesjs-preset-webpage/dist/grapesjs-preset-webpage.min.css';
@@ -7,22 +7,46 @@ import 'grapesjs/dist/grapes.min.js';
 import 'grapesjs-preset-webpage/dist/grapesjs-preset-webpage.min.js';
 import { render } from "react-dom";
 
-class App extends Component{
-  componentDidMount(){
+// class App extends Component{
+//   componentDidMount(){
     
-      grapesjs.init({
-        container: '#gjs',
-        pugins: ['gjs-preset-webpage'],
-      })
-    }
+//       grapesjs.init({
+//         container: '#gjs',
+//         pugins: ['gjs-preset-webpage'],
+//       })
+//     }
 
-  render(){
-    return (
-    <Fragment>
-      <div id="gjs"></div>
-    </Fragment>
-    );
-  }
+//   render(){
+//     return (
+//     <Fragment>
+//       <div id="gjs"></div>
+//     </Fragment>
+//     );
+//   }
+// }
+
+// export default App;
+
+function App(){
+
+  const [editor, setEditor] = useState(null);
+  useEffect(()=>{
+    const editor =  grapesjs.init({
+        container: '#gjs',
+        plugins: ['gjs-preset-webpage'],
+         });
+         setEditor(editor);
+
+  },[]);
+
+  return <div className="App">
+    <div id="editor">
+    <div id="gjs">
+      
+    </div>
+    </div>
+
+  </div>
 }
 
 export default App;
